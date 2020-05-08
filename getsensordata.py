456c7154e11dfdf7e89a4be353e8b60465fc1d7e
jwf19 <james.flemington@outlook.com>
2020-05-08 15:50:19 +0100

    added cpu temp and date

--- getsensordata.py
+++ getsensordata.py
@@ -1,12 +1,16 @@
 import Adafruit_DHT
 import yaml
+from gpiozero import CPUTemperature
+from datetime import datetime
 
-with open("sensor_config.yaml", "r") as cfg:
+with open("/home/pi/code/climate-sensor/sensor_config.yaml", "r") as cfg:
 	config = yaml.load(cfg, Loader=yaml.FullLoader)
 
 SENSOR_LOCATION_NAME = config["SENSOR_LOCATION_NAME"]
 
 humidity, temp_c = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
-format_args = ["SENSOR", SENSOR_LOCATION_NAME, "TEMPERATURE", temp_c,
-	       "HUMIDITY", humidity]
-print("\n{:<20}: {}\n{:<20}: {:.2f} C\n{:<20}: {:.2f} %\n".format(*format_args))
+cpu_temp = CPUTemperature().temperature
+date = datetime.now()
+format_args = ["PI TIME", date.strftime("%Y-%m-%d %H:%M:%S"), "SENSOR", SENSOR_LOCATION_NAME, "TEMPERATURE", temp_c,
+	       "HUMIDITY", humidity, "CPU TEMPERATURE", cpu_temp]
+print("\n{:<20}: {}\n{:<20}: {}\n{:<20}: {:.2f} C\n{:<20}: {:.2f} %\n{:<20}: {:.2f} C\n".format(*format_args))
