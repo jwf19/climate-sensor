e5c7c9513a950a510a2e22ee308e591474309279
jwf19 <james.flemington@outlook.com>
2020-05-08 18:14:55 +0100

    added CPU temperature

--- tempsensor.py
+++ tempsensor.py
@@ -1,12 +1,14 @@
 import Adafruit_DHT
 from ISStreamer.Streamer import Streamer
 import time
+from datetime import datetime
 import yaml
+from gpiozero import CPUTemperature
 
 
 # ------- User Settings ---------
-with open("sensor_config.yaml", "r") as cfg:
-	config  = yaml.load(cfg, loader=yaml.FullLoader)
+with open("/home/pi/code/climate-sensor/sensor_config.yaml", "r") as cfg:
+	config  = yaml.load(cfg, Loader=yaml.FullLoader)
 
 SENSOR_LOCATION_NAME = config["SENSOR_LOCATION_NAME"]
 BUCKET_NAME = config["BUCKET_NAME"]
@@ -18,6 +20,11 @@ METRIC_UNITS = config["METRIC_UNITS"]
 
 streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 while True:
+	sys_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	cpu_temp = CPUTemperature().temperature
+	streamer.log(SENSOR_LOCATION_NAME + " CPU Temperature(C)", format(cpu_temp, ".2f"))
+	print("\n{} System Time {}".format(SENSOR_LOCATION_NAME, sys_time))
+	print("{} CPU Temperature {:.2f} C".format(SENSOR_LOCATION_NAME, cpu_temp))
 	humidity, temp_c = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
 	if METRIC_UNITS:
 		streamer.log(SENSOR_LOCATION_NAME + " Temperature(C)", format(temp_c, ".2f"))
